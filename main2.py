5d0ecff6249e2105eb231aaf759bbda64077b511
AlexHamre <91455968+AlexHamre@users.noreply.github.com>
2021-10-11 01:09:06 +0200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,97 @@
+# importing math and setting up a loop
+import math
+
+running = True
+print("type exit to exit")
+print("this calculator has +,-,*,/,!,sqrt,^,and more later")
+while running:
+    # the input
+    a = input("write math: ")
+    # code so you can exit
+    if a == "exit":
+        running = False
+        print("exiting")
+    else:
+        # finds operators
+        b = a.find("+")
+        c = a.find("-")
+        d = a.find("*")
+        e = a.find("/")
+        f = a.find("^")
+        g = a.find("sqrt")
+        h = a.find("!")
+
+        # checks if you enter blank
+        if a == "":
+            print("Error-blank")
+
+        # checks for operator and does appropriate math
+        elif a[b] == "+":
+            # pre_op is all numbers in a from zero to + (b in this case)
+            pre_op = a[:b]
+            # post_op is everything after +
+            post_op = a[b + 1:]
+            # This checks if it can properly add your numbers together
+            try:
+                answer = float(pre_op) + float(post_op)
+                # If it can't add them it prints "Error-plus"
+            except:
+                print("Error-plus")
+
+        # the rest is nearly the same as addition
+        elif a[c] == "-":
+            pre_op = a[:c]
+            post_op = a[c + 1:]
+            try:
+                answer = float(pre_op) - float(post_op)
+            except:
+                print("Error-minus")
+
+        elif a[d] == "*":
+            pre_op = a[:d]
+            post_op = a[d + 1:]
+            try:
+                answer = float(pre_op) * float(post_op)
+            except:
+                print("Error-multiply")
+
+        elif a[f] == "^":
+            pre_op = a[:f]
+            post_op = a[f + 1:]
+            try:
+                answer = float(pre_op) ** float(post_op)
+            except:
+                print("Error-power")
+
+        elif a[g] == "s":
+            pre_op = a[:g]
+            try:
+                answer = math.sqrt(float(pre_op))
+            except:
+                print("Error-Square")
+
+        elif a[h] == "!":
+            pre_op = a[:h]
+            try:
+                pre_op = int(pre_op)
+                answer = (math.factorial(pre_op))
+            except:
+                print("Error-factorial")
+
+        elif a[e] == "/":
+            pre_op = a[:e]
+            post_op = a[e + 1:]
+            try:
+                answer = float(pre_op) / float(post_op)
+            except:
+                print("Error-div")
+        # Tries to print the answer
+        try:
+            print(answer)
+        # if the answer is undefined it prints an error
+        except:
+            print("Error-no answer")
+
+        # makes the answer undefined again
+        answer = ""
+        del answer
